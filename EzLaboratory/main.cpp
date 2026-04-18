4645062e14572aab2d089b01959f75bb596af2d5
Shulin <shulincookie@gmail.com>
2026-04-18 10:35:15 +0800

    new

--- EzLaboratory/main.cpp
+++ EzLaboratory/main.cpp
@@ -1,10 +1,10 @@
-#include "MainWindow.h"
+#include "EzLaboratory.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-    MainWindow window;
+    EzLaboratory window;
     window.show();
     return app.exec();
 }
